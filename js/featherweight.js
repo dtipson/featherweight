c58511cb505abfbc991794cbd8df7412ca1b5c95
Drew Tipson <dtipson@gmail.com>
2013-02-10 16:34:10 -0500

    raw

--- js/featherweight.js
+++ js/featherweight.js
@@ -21,7 +21,7 @@
 				(nonyt)?
 					('https://player.vimeo.com/video/'+slug+'?'+$.param(ops)):
 					('https://www.youtube.com/embed/'+slug+'?'+$.param(ops))
-		)+'" style="background:black;" frameborder="0" allowfullscreen></iframe>';
+		)+'" style="background:black;" frameborder="0" allowfullscreen allowtransparency="true"></iframe>';
 	}
 	
 	function vidProcess($el){
